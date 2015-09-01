df4cbfad1c629b5655b07799c982b889668865e6
mateuszanatta <mateus_zanatta@hotmail.com>
2015-09-01 11:20:30 -0600

    First Commit

--- app/Cities.php
+++ app/Cities.php
@@ -6,5 +6,7 @@ use Jenssegers\Mongodb\Model as Eloquent;
 
 class Cities extends Model
 {
-    //
+  protected $collection = "cities";
+  protected $primaryKey  = "_id";
+  public $timestamps = false;
 }
