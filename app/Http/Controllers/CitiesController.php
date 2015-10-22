5dbf73c584113109b786f81d12406baa6c4ddc1a
mateuszanatta <mateus_zanatta@hotmail.com>
2015-10-22 14:33:01 -0600

    Applied z-transform

--- app/Http/Controllers/CitiesController.php
+++ app/Http/Controllers/CitiesController.php
@@ -6,35 +6,57 @@ class CitiesController extends Controller{
 
   public function index()
   {
-    // $data = Cities::where("Municipio", "Alpestre")->take(1)->get();
-    // $data = Cities::all();
-
-    if(isset($_GET)){
-      $columns = $_GET["vars"];
-
-      $var = '';
-      foreach ($columns as $key => $value) {
-        $var = $var.$value.'.';
-      }
-
-      $data = CitiesController::selectData(array(
-                      $var => true,
-                      'Municipio' => true,
-                      "_id" => false));
-      return $data;
-    }else{
-      return "Nenhuma variável foi selecionada";
-    }
+    return view('cities');
   }
 
   /**
-  Define which columns will be queried in the database
-  @param array $columns
-  @return result of the query executed in MongoDB
+  * Define which columns will be queried in the database
+  * @param array $columns
+  * @return result of the query executed in MongoDB
   */
   public function selectData($columns){
     $data = Cities::project($columns)->orderBy("Municipio","asc")->get();
 
     return $data;
+  }
+  public function category(){
+    // $data = Cities::where("Municipio", "Alpestre")->take(1)->get();
+    // $data = Cities::all();
+
+    //It is going to receive a GET request with a array that defines what variableas are being requested
+    //Ex.: localhost:8080/public/cities?vars[]=Domicilios_Particulares_Permanentes&vars[]=Por_Abastecimento_de_Agua&vars[]=Rede_Geral
+    if(isset($_GET)){
+      $columns = $_GET["selected"];
+
+      $var = '';
+      //Concatenate the variables from the GET request into a string to be used as a query on the database
+      //Ex.:Domicilios_Particulares_Permanentes.Por_Abastecimento_de_Agua.Rede_Geral
+      foreach ($columns as $key => $value) {
+        $var = $var.$value.'.';
+      }
+
+      //It will apply the query into the database and return the data
+      $data = CitiesController::selectData(array(
+                      $var => true,
+                      'Municipio' => true,
+                      "_id" => false));
+      // $keys = Cities::raw()->findOne(array('Domicilios_Particulares_Permanentes' =>array(
+      //                 $var => true,
+      //                 'Municipio' => true,
+      //                 "_id" => false)));
+
+      $data2 = array();
+      foreach($data as $key => $value){
+        array_push($data2, array($value['Municipio'] => $value[$columns[0]]));
+      }
+      //$data2 = json_encode($data);
+      //echo '<pre>';
+      // print_r($data[0]['Domicilios_Particulares_Permanentes']);
+      //print_r($data2[0]);
+      //echo '</pre>';
+      return $data2;
+    }else{
+      return "Nenhuma variável foi selecionada";
+    }
   }
 }
